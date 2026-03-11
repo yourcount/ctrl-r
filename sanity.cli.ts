2fbb7a60f232068961ac473862be5b0b24379375
yourcount <t.degraaf13@gmail.com>
2026-03-11 18:50:20 +0100

    Configure hosted Sanity Studio deployment and ignore studio artifacts

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,4 +5,7 @@ export default defineCliConfig({
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? "",
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? "production",
   },
+  deployment: {
+    appId: "nt0ywbnu3q1j29xer63fmfjx",
+  },
 });
